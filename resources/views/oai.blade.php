63ce0a26eef8217cee39152aa07e36dea42f4e24
Lopezbit <fabi.lopez1992@gmail.com>
2021-03-02 17:10:18 -0500

    inic

--- resources/views/oai.blade.php
+++ resources/views/oai.blade.php
@@ -7,4 +7,16 @@
          <responseDate>2002-02-08T12:00:01Z</responseDate>
          <request verb="Identify">http://metada.persontechnology.com/libro/oai</request>
 
+         <Identify>
+            <repositoryName>Libros Person</repositoryName>
+            <baseURL>http://metada.persontechnology.com/libro/oai</baseURL>
+            <protocolVersion>2.0</protocolVersion>
+            <adminEmail>fabi.lopez1992@gmail.com</adminEmail>
+            <earliestDatestamp>2018-01-10T22:22:43Z</earliestDatestamp>
+            <deletedRecord>transient</deletedRecord>
+            <granularity>YYYY-MM-DDThh:mm:ssZ</granularity>
+            <compression>deflate</compression>
+            
+         </Identify>
+
 </OAI-PMH>
